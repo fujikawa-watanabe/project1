1ad799887570f57400926d704a634a2da8c88bbf
yuichi-watanabe <16t2178h@shinshu-u.ac.jp>
2019-01-22 22:22:13 +0900

    tama

---
+++ Unity chan! Project/Assets/tamascript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tamascript : MonoBehaviour {
+    
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Invoke("kesu", 1.5f);
+    }
+    void kesu()
+    {
+        Destroy(gameObject);
+    }
+}
